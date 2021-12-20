ed05984df668fe1e5521ea89cdf40a827637a192
illupovero <davideluporini1996@gmail.com>
2021-12-20 15:37:39 +0100

    ended base version

--- script.js
+++ script.js
@@ -9,16 +9,29 @@ console.log('ok js');
 
 // 1) recupero il nome dall'utente
 // 2) recupero il cognome dall'utente
-// 3) recupero l'età dall'utente
-// 4) aggiungo testo+21
-// 5) risultato
+// 3) recupero colore preferito dall'utente
+// 4) recupero elemento da stampare
+// 5) aggiungo testo+21
+// 6) stampo
 
 
 // recupero nome
-const Name = prompt('Qual è il tuo nome?')
-console.log(Name)
+const Name = prompt('Qual è il tuo nome?');
+console.log(Name);
 
 // recupero cognome
 
-const Surname = prompt('Qual è il tuo cognome?')
-console.log(Surname)
+const Surname = prompt('Qual è il tuo cognome?');
+console.log(Surname);
+
+// recupero colore preferito dall'utente
+
+const favouriteColor = prompt('colore preferito?');
+console.log(favouriteColor);
+
+// recupero elemento da stampare
+const password = document.getElementById('password');
+console.log(password);
+
+// stampo e aggiungo 21 nel testo
+password.innerHTML = `La tua password è:  ${Name}${Surname}${favouriteColor}21`;
