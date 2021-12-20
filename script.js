29a307006a9c60d2ae3416c096b242eee9a9fa8f
illupovero <davideluporini1996@gmail.com>
2021-12-20 15:16:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('ok js');
+
+// creare generatoere di password dove chiedere:
+
+// Name
+// cognome
+// età
+// aggiungere testo+21
+
+// 1) recupero il nome dall'utente
+// 2) recupero il cognome dall'utente
+// 3) recupero l'età dall'utente
+// 4) aggiungo testo+21
+// 5) risultato
